1ddc8d4ba2b852d144874e84d0906cb7fe870719
hussien tawhidi <hussientawhidi710@gmail.com>
2025-10-15 18:05:54 +0330

    updated

--- src/components/MainBg.tsx
+++ src/components/MainBg.tsx
@@ -1,7 +1,13 @@
-<div className='fixed inset-0 z-[-1]'>
-  <div
-    className='absolute inset-0 bg-center bg-cover'
-    style={{ backgroundImage: 'url("/assets/hero.jpg")' }}
-  />
-  <div className='absolute inset-0 bg-my-bg/80' />
-</div>;
+"use client";
+
+export default function MainBg() {
+  return (
+    <div className='fixed inset-0 z-[-1]'>
+      <div
+        className='absolute inset-0 bg-center bg-cover'
+        style={{ backgroundImage: 'url("/assets/hero.jpg")' }}
+      />
+      <div className='absolute inset-0 bg-my-bg/80' />
+    </div>
+  );
+}
