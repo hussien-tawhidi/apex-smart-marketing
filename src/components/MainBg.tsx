c5cc92efcc7b27908a97d7639628bbe4a8ebcceb
hussien tawhidi <hussientawhidi710@gmail.com>
2025-10-15 18:04:39 +0330

    updated

--- src/components/MainBg.tsx
+++ src/components/MainBg.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-export default function MainBg() {
-  return (
-    <div className='fixed inset-0 z-[-1]'>
-      {/* Background image */}
-      <div
-        className='absolute inset-0 bg-center bg-cover'
-        style={{ backgroundImage: 'url("/assets/hero.jpg")' }}
-      />
-
-      {/* Overlay */}
-      <div className='absolute inset-0 bg-my-bg/80' />
-      {/* 
-        bg-black/40 = black with 40% opacity
-        Change to bg-my-overlay/50 or any Tailwind color you want 
-      */}
-    </div>
-  );
-}
+<div className='fixed inset-0 z-[-1]'>
+  <div
+    className='absolute inset-0 bg-center bg-cover'
+    style={{ backgroundImage: 'url("/assets/hero.jpg")' }}
+  />
+  <div className='absolute inset-0 bg-my-bg/80' />
+</div>;
